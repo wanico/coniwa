ea704ac004e39938f7b81ac4f95df534302949f4
yoshimura <yoshimura@stitch.lassic.local>
2012-10-03 14:53:24 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coniwa::Application.config.secret_token = '92d7b0de52fe5ec1918d18109c3d0acb52555de425eecb26425a967368508cadf6cefae46292a6e8f9cec85ff4088dee9dd894a6a2d96923b86285ef68ab06d6'
