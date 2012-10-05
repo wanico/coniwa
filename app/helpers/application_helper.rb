f751c7ff908f74a73b53d3316b9636e99d7a418c
yoshimura <yoshimura@stitch.lassic.local>
2012-10-05 16:38:50 +0900

    add Login Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# -*- coding: utf-8 -*-
+
 module ApplicationHelper
+  def if_login
+    yield if params[:controller] == 'login'
+  end
+  def unless_login
+    yield unless params[:controller] == 'login'
+  end
 end
